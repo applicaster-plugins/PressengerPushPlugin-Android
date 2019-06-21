72d9bd3409dd256d993376225403e9eca006d4f5
Juan Hernandez <j.hernandez@applicaster.com>
2019-06-21 14:31:40 -0400

    already implemented (now will start looking for what is wrong)

--- src/main/java/com/applicaster/pressengerpushplugin/PressengerPushProvider.kt
+++ src/main/java/com/applicaster/pressengerpushplugin/PressengerPushProvider.kt
@@ -1,22 +1,42 @@
 package com.applicaster.pressengerpushplugin
 
 import android.content.Context
+import android.util.Log
 import com.applicaster.plugin_manager.push_plugin.PushContract
 import com.applicaster.plugin_manager.push_plugin.helper.PushPluginsType
 import com.applicaster.plugin_manager.push_plugin.listeners.PushTagLoadedI
 import com.applicaster.plugin_manager.push_plugin.listeners.PushTagRegistrationI
+import com.pressenger.sdk.PressengerSDKFactory
+import com.pressenger.sdk.utils.callback.Callback
+import com.pressenger.sdk.utils.callback.Result
 
 class PressengerPushProvider : PushContract {
+
+    var groupId: String? = null
+
     override fun registerPushProvider(context: Context?, registerID: String?) {
-        // todo: implement this
+        // do nothing
     }
 
     override fun initPushProvider(context: Context?) {
         // todo: implement this
+        val instance = PressengerSDKFactory.getInstance()
+        instance.checkOverlayEnabled(context)
+        instance.register(context, groupId) {
+            result ->
+            if (result.resultCode == Result.RES_SUCCESS) {
+                Log.d(this.javaClass.simpleName, "push provider initialized successfully")
+            } else {
+                Log.e(this.javaClass.simpleName, "push provider initialization failed")
+            }
+        }
     }
 
     override fun setPluginParams(params: MutableMap<Any?, Any?>?) {
         // todo: implement this
+        params?.let {
+            groupId = params["group_id"].toString()
+        }
     }
 
     override fun addTagToProvider(context: Context?, tag: MutableList<String>?, pushTagRegistrationListener: PushTagRegistrationI?) {
